8dff3b8e2b223e8826294f367f05364e0250984a
sondmfpt <sondmhe186291@fpt.edu.vn>
2025-06-16 18:20:47 +0700

    add card for view

--- src/components/view/ViewContent.tsx
+++ src/components/view/ViewContent.tsx
@@ -1,3 +1,7 @@
+import { Button, Card, CardBody, CardFooter, CardHeader, Divider, Image } from "@heroui/react";
+import { FaRegComment, FaRegHeart } from "react-icons/fa";
+import { RiShareForwardLine } from "react-icons/ri";
+
 import styled from "styled-components";
 
 export const ViewContent = () => {
@@ -9,6 +13,34 @@ export const ViewContent = () => {
                     Chào mừng đến với nhật ký cá nhân của tôi. Tại đây tôi chia sẻ những suy nghĩ, kinh nghiệm và suy ngẫm của mình về hành trình cuộc sống.
                 </p>
             </ViewContentHeader>
+            <ViewContentBody className="flex flex-col gap-4 mt-4">
+                <Card className="w-full">
+                    <CardHeader className="w-full h-64 p-0 justify-center overflow-hidden">
+                        <Image
+                            src="https://media.istockphoto.com/id/1457889029/photo/group-of-food-with-high-content-of-dietary-fiber-arranged-side-by-side.jpg?s=612x612&w=0&k=20&c=SEyObHsbBsrd1XZlgEg389VT86BMFKZKfKReKyVPAk4="
+                            alt="food"
+                            width={5000}
+                        />
+                    </CardHeader>
+                    <CardBody>
+                        <p className="text-gray-500">
+                            Đây là bài viết 1.
+                        </p>
+                    </CardBody>
+                    <Divider />
+                    <CardFooter className="flex justify-evenly items-center">
+                        <Button startContent={<FaRegHeart />}>
+                            Like
+                        </Button>
+                        <Button startContent={<FaRegComment />}>
+                            Bình luận
+                        </Button>
+                        <Button startContent={<RiShareForwardLine />}>
+                            Chia sẻ
+                        </Button>
+                    </CardFooter>
+                </Card>
+            </ViewContentBody>
         </ViewContentWrapper>
     )
 }
@@ -18,3 +50,5 @@ const ViewContentWrapper = styled.div`
 `;
 
 const ViewContentHeader = styled.div``
+
+const ViewContentBody = styled.div``
